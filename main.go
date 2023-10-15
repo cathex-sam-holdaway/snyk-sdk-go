4b728e57ee8f19e1ee661b722610dffb82bc8e72
Sam Holdaway <sam.holdaway@checkatrade.com>
2023-10-15 20:12:15 +0100

    DEVX-729 - Add a simple program to allow me to test the new targets function

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cathex-sam-holdaway/snyk-sdk-go/snyk"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Snyk struct {
+	Client       *snyk.Client
+	SnykApiToken string `envconfig:"SNYK_API_TOKEN" required:"true"`
+	DummyVar     string `envconfig:"DUMMY_VAR" required:"true"`
+}
+
+func main() {
+	var s Snyk
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("error loading .env file : %v", err)
+	}
+
+	if err := envconfig.Process("", &s); err != nil {
+		fmt.Printf("error getting environment variables : %v", err)
+	}
+
+	snykClient := snyk.NewClient(s.SnykApiToken)
+
+	targets, _, err := snykClient.Targets.List(context.Background(), "23ec072e-580e-4e16-bd64-856d3bad9aac")
+
+	if err != nil {
+		fmt.Printf("error getting targets : %v", err)
+	}
+
+	fmt.Println(targets)
+}
